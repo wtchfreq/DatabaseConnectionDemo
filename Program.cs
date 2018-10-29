eaa14e4aa5b5bf9e16eeb1c041a1c67d076879fb
Kostiantyn Bogomazov <kost.bogomazov@gmail.com>
2018-10-29 18:35:11 +0200

    First commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatabaseConnectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
