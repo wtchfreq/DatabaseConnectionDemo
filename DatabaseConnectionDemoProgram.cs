35a06f038153cf77d8ae90f39ee4dc3692a44899
Kostiantyn Bogomazov <kost.bogomazov@gmail.com>
2018-10-29 19:50:12 +0200

    Made working connection to Sql Server DB.

---
+++ DatabaseConnectionDemoProgram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DatabaseConnectionDemo
+{
+    class DatabaseConnectionDemoProgram
+    {
+
+        static void Main(string[] args)
+        {
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder["Server"] = @"IRWINE\SQLEXPRESS";
+            builder["Database"] = @"COMPANY";
+            builder["User Id"] = @"sharpdbacc";
+            builder["Password"] = @"sharpdbacc";
+
+
+            using (var conn = new SqlConnection(builder.ConnectionString))
+            {
+                var command = new SqlCommand("SELECT * FROM Employee", conn);
+                conn.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        Console.WriteLine($"{reader[0]} {reader[1]}");
+                    }
+                }
+            }
+
+        }
+    }
+}
